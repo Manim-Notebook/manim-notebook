021384dc322469bd3c4f7c6a24db79d5296aeb50
Splines <dominic-plein@gmx.de>
2024-12-27 02:54:12 +0100

    Reconfigure tsconfig & prepare test setup

--- .config/.vscode-test.mjs
+++ .config/.vscode-test.mjs
@@ -2,4 +2,7 @@ import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
   files: "out/test/**/*.test.js",
+  version: "stable",
+  workspaceFolder: "tests/fixtures",
+  extensionDevelopmentPath: "../",
 });
