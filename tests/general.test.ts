3ef24fecb2a88a558264913d193d57c2782496b8
Splines <dominic-plein@gmx.de>
2024-12-27 04:56:21 +0100

    Try some sample tests

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -1,13 +1,22 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
 import { window } from "vscode";
-import * as note from "../src/extension";
+// import * as note from "../src/extension";
+import { before } from "mocha";
 
-suite("Extension Test Suite", () => {
-  window.showInformationMessage("Start all tests.");
+suite("General Tests", () => {
+  before(() => {
+    window.showInformationMessage("Start all tests.");
+  });
 
-  test("Sample test", () => {
+  test("Sample test", async () => {
     assert.strictEqual(-1, [1, 2, 3].indexOf(5));
     assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+
+    const terminal = await window.createTerminal("yo");
+    terminal.show();
+    terminal.sendText("ls");
+
+    await window.showInformationMessage("Sample test passed.");
   });
 });
