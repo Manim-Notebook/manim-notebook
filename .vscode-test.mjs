33adff2bdc3aaa09d730f103dbbf44cf5907cce7
Splines <dominic-plein@gmx.de>
2024-12-27 17:12:18 +0100

    Remove vscode test config

--- .vscode-test.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@vscode/test-cli";
-
-export default defineConfig({
-  files: "out/test/**/*.test.js",
-  version: "stable",
-  workspaceFolder: "tests/fixtures",
-  // https://github.com/microsoft/vscode-extension-test-runner/issues/56
-  // extensionDevelopmentPath: "../"
-});
