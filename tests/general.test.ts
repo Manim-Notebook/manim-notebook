1b63bf5d4d8e994603128b85b6eca2efb7ddd183
Splines <dominic-plein@gmx.de>
2024-12-27 15:31:30 +0100

    Add minimal reproducible test example

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -1,22 +1,17 @@
-import * as assert from "assert";
-import * as vscode from "vscode";
 import { window } from "vscode";
-// import * as note from "../src/extension";
 import { before } from "mocha";
 
-suite("General Tests", () => {
+suite("Sample test", () => {
   before(() => {
     window.showInformationMessage("Start all tests.");
   });
 
   test("Sample test", async () => {
-    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-
     const terminal = await window.createTerminal("yo");
     terminal.show();
-    terminal.sendText("ls");
+    terminal.sendText("ls -la");
 
-    await window.showInformationMessage("Sample test passed.");
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log("Done");
   });
 });
