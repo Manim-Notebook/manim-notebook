743a2ed22c0001e4dd59d7206db77f036f46327a
Splines <dominic-plein@gmx.de>
2024-12-31 00:52:49 +0100

    Clean up dummy test

--- tests/general.test.ts
+++ tests/general.test.ts
@@ -1,21 +1,12 @@
-import * as vscode from "vscode";
-import { window } from "vscode";
-
 import { describe, it } from "mocha";
-import { before } from "mocha";
-import * as assert from "assert";
 
+// eslint-disable-next-line no-unused-vars
 import * as manimNotebook from "@src/extension";
-// import { manimCaller } from "./utils/testRunner";
 
 describe("Sample test", function () {
-  before(async () => {
-    // https://github.com/microsoft/vscode-discussions/discussions/2468
-  });
-
   it.only("Manim test", async () => {
-    console.log("Running test");
-    await new Promise(resolve => setTimeout(resolve, 40000));
-    console.log("Test done");
+    console.log("Running test...");
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log("Test finished");
   });
 });
