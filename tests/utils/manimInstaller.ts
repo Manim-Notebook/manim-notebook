13d62433864b9796b7d970d1937cb81516d28907
Splines <dominic-plein@gmx.de>
2024-12-31 01:17:14 +0100

    Shallow clone Manim repo

--- tests/utils/manimInstaller.ts
+++ tests/utils/manimInstaller.ts
@@ -78,7 +78,7 @@ export class ManimInstaller {
     console.log("🎁 Downloading Manim... (this might take a while)");
     // 2>&1 redirects stderr to stdout since git writes to stderr for
     // diagnostic messages and we don't want to reject the promise in that case.
-    await run(`git clone https://github.com/3b1b/manim.git ${this.manimPath} 2>&1`,
+    await run(`git clone --depth 1 https://github.com/3b1b/manim.git ${this.manimPath} 2>&1`,
       { cwd: this.manimPath });
   }
 
