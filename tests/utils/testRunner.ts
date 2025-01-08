06edca72c949375a7bf2b42287a4b8942f1faebc
Splines <dominic-plein@gmx.de>
2025-01-08 12:30:59 +0100

    Increase Mocha timeout

--- tests/utils/testRunner.ts
+++ tests/utils/testRunner.ts
@@ -40,7 +40,7 @@ export function uriRelative(pathRelativeToWorkspaceRoot: string): Uri {
 export function run(): Promise<void> {
   const mocha = new Mocha({
     ui: "tdd",
-    timeout: 35000,
+    timeout: 60000,
   });
 
   console.log(`💠 workspaceRoot: ${WORKSPACE_ROOT}`);
