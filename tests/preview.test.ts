b235adfffd74fb54844cc7bd8111a4acc5e2434a
Splines <dominic-plein@gmx.de>
2025-01-19 15:47:46 +0100

    Add laggy patched preview test & test sample

--- tests/preview.test.ts
+++ tests/preview.test.ts
@@ -1,22 +1,74 @@
 import { window, commands } from "vscode";
-import { describe, it } from "mocha";
+import { describe, it, before } from "mocha";
 import { onAnyTerminalOutput } from "./utils/terminal";
 import { uriInWorkspace } from "./utils/testRunner";
 import { goToLine } from "./utils/editor";
+let expect: Chai.ExpectStatic;
+
+before(async () => {
+  // why this weird import syntax?
+  // -> see https://github.com/microsoft/vscode/issues/130367
+  const chai = await import("chai");
+  expect = chai.expect;
+});
 
 describe("Previewing", function () {
   it("Can preview the current Manim Cell", async () => {
-    const editor = await window.showTextDocument(uriInWorkspace("basic.py"));
-    goToLine(editor, 11);
     await commands.executeCommand("manim-notebook.previewManimCell");
 
     return new Promise((resolve) => {
-      onAnyTerminalOutput((data, stopListening) => {
+      onAnyTerminalOutput(async (data, stopListening) => {
         if (data.includes("ReplacementTransformCircle")) {
           stopListening();
-          resolve();
+          await commands.executeCommand("manim-notebook.exitScene");
+          setTimeout(resolve, 1500);
         }
       });
     });
+  });
+
+  it.only("Can preview laggy scene", async () => {
+    const editor = await window.showTextDocument(uriInWorkspace("laggy-patched.py"));
+    const queue: { line: number; waitForStrings: string[]; resolve: () => void }[] = [];
+    let wantToStopListening = false;
+
+    onAnyTerminalOutput(async (data, stopListening) => {
+      if (wantToStopListening) {
+        stopListening();
+        return;
+      }
+      if (queue.length === 0) {
+        throw new Error("Listening to terminal output, but nothing in queue to check against");
+      }
+
+      const { waitForStrings } = queue[0];
+      for (const str of waitForStrings) {
+        if (data.includes(str)) {
+          waitForStrings.removeByValue(str);
+        }
+      }
+      if (waitForStrings.length === 0) {
+        queue.shift()?.resolve();
+      }
+    });
+
+    async function testPreviewAtLine(line: number, waitForStrings: string[]) {
+      goToLine(editor, line);
+      await commands.executeCommand("manim-notebook.previewManimCell");
+      await new Promise<void>((resolve) => {
+        queue.push({ line, waitForStrings, resolve });
+      });
+    }
+
+    await testPreviewAtLine(44, ["ShowCreationVGroup", "In [2]:"]);
+    await testPreviewAtLine(50, ["_MethodAnimationValueTracker", "In [3]:"]);
+    await testPreviewAtLine(57, ["_MethodAnimationValueTracker", "In [4]:"]);
+    await testPreviewAtLine(50, ["_MethodAnimationValueTracker", "In [5]:"]);
+
+    wantToStopListening = true; // cleanup for subsequent tests
+    expect(queue.length).to.equal(0);
+
+    await commands.executeCommand("manim-notebook.exitScene");
+    await new Promise(resolve => setTimeout(resolve, 1000));
   });
 });
