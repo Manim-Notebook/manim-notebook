5435532527aac6f4790dbccb51e18cbb96c1e9bd
Splines <dominic-plein@gmx.de>
2025-01-19 12:53:28 +0100

    Use \x1b\r\r at end

--- src/manimShell.ts
+++ src/manimShell.ts
@@ -367,15 +367,16 @@ export class ManimShell {
 
     let currentExecutionCount = this.iPythonCellCount;
 
-    /*  if (process.platform === "win32") {
+    if (process.platform === "win32") {
       this.detectShellExecutionEnd = false;
       // https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-Bracketed-Paste-Mode
-      shell.sendText(`\x1b[201~\x1b[?2004l\u000F${command}\x1b\r\n\r\n`, false);
+      // shell.sendText(
+      // `\x1b[201~\x1b[?2004l\u000F${command}\x1b\r\n\r\n`, false);
+      shell.sendText(`${command}\x1b\r\r`, false);
       this.detectShellExecutionEnd = true;
     } else {
       this.exec(shell, command);
-    } */
-    this.exec(shell, command);
+    }
 
     handler?.onCommandIssued?.(this.activeShell !== null);
 
