2bf6779db9ec93ea26401f8a93dbfc3fcf0d451f
Splines <dominic-plein@gmx.de>
2025-01-20 17:17:06 +0100

    Fix wrong filename in test

--- tests/preview.test.ts
+++ tests/preview.test.ts
@@ -28,7 +28,7 @@ describe("Previewing", function () {
   });
 
   it.only("Can preview laggy scene", async () => {
-    const editor = await window.showTextDocument(uriInWorkspace("laggy-patched.py"));
+    const editor = await window.showTextDocument(uriInWorkspace("laggy.py"));
     const queue: { line: number; waitForStrings: string[]; resolve: () => void }[] = [];
     let wantToStopListening = false;
 
